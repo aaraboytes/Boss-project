ad6316e6fb7e8c1c3c062932737dbcb896d7af3b
chetobros <chetobros@gmail.com>
2019-05-19 15:40:08 -0500

    Boss added

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss : MonoBehaviour
+{
+    [SerializeField] int life;
+    int currentLife;
+    [SerializeField] Slider sliderHealth;
+    Animator anim;
+    void Start()
+    {
+        currentLife = life;
+        sliderHealth.value = 1;
+        anim = GetComponent<Animator>();
+    }
+    public void Damage()
+    {
+        currentLife--;
+        sliderHealth.value = currentLife / life;
+        if (currentLife<= 0) {
+            Die();
+            return;
+        }
+    }
+    void Die()
+    {
+        anim.SetTrigger("Die");
+    }
+}
